97af6c48c1c59501139b2ab781b71be64a6c6dac
jeromebraure-tech <jeromebraure@gmail.com>
2026-09-03 19:25:53 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.HORIZON_CONFIG = {
-  sheetEndpoint: ''
+  sheetEndpoint: 'https://script.google.com/macros/s/AKfycbwQcea_oIev4cHai_wZZT6itVycMCLiFk2qTTyad7q90pziLB_rDdhX16f5EGWENR8e4w/exec'
 };
